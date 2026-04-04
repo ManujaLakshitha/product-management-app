3f84a653309b543e4219d1981e26398b16b592d3
ManujaLakshitha <rajinda1205@gmail.com>
2026-04-04 15:16:04 +0530

    display product list on home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,10 @@
-import Image from "next/image";
-import styles from "./page.module.css";
-import './globals.css';
+import ProductList from "../components/ProductList";
 
 export default function Home() {
   return (
-    <main className="flex flex-col items-center justify-center min-h-screen p-4">
-      <h1 className="text-xl font-bold text-blue-600 mb-4">
-        Tailwind CSS Test
-      </h1>
-      <p className="text-lg text-gray-700">
-        If you see this styled text, Tailwind CSS is working!
-      </p>
-      <button className="mt-6 px-6 py-2 bg-green-500 text-white rounded-lg hover:bg-green-600 transition">
-        Click Me
-      </button>
+    <main className="p-4">
+      <h1 className="text-3xl font-bold mb-4">Products</h1>
+      <ProductList />
     </main>
   );
 }
